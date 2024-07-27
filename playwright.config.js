df48110a416bc219e8e4219fe8e73e8dc1c04e1e
Subhajit Chowdhury <dev.subhajitchow@gmail.com>
2024-07-27 14:14:31 +0900

    commit 27/07/2024

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ const config = {
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: 1,
-    testDir: "tests",
+    testDir: "./tests",
     testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
     timeout: 80000,
 
@@ -19,7 +19,7 @@ const config = {
         browserName: "chromium",
         headless: false,
 
-        baseURL: "https://admin-advertisement.herokuapp.com",
+        // baseURL: "https://admin-advertisement.herokuapp.com",
 
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: false,
--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,167 +1,45 @@
 const { test, expect } = require("@playwright/test");
 const AxeBuilder = require("@axe-core/playwright").default;
-const { MainPage } = require("../lib/pageobjects/LoginPage");
+const { LoginPage } = require("../lib/pageobjects/LoginPage");
+const path = require('path');
 
-test.beforeAll(async ({ request }) => {
-    const req = await request.get("/api/advertisements/db/drop?confirm=y");
-    expect(req.ok()).toBeTruthy();
-});
+const fs = require('fs');
+const projectPath = process.cwd();
+console.log(projectPath);
 
-test("UI accessibility test", async ({ page }, testInfo) => {
-    const mainPage = new MainPage(page);
-    await mainPage.goto();
+const filePath = path.join(projectPath, 'data/credentials.json');
+console.log(filePath);
+// Read credentials from JSON file
+const credentials = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+test.describe("ui functional Test", () => {
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-    await testInfo.attach("accessibility-scan-results", {
-        body: JSON.stringify(accessibilityScanResults, null, 2),
-        contentType: "application/json",
+    test("Login test", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto(credentials.url,credentials.userID,credentials.password);
+        //expect(accessibilityScanResults.violations).toEqual([]);
     });
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-});
-
-test("Page main elements in place", async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.goto();
-    await expect(mainPage.appNameText).toBeVisible();
-    await expect(mainPage.newItemButton).toBeVisible();
-    await expect(mainPage.tableHeader.name).toBeVisible();
-    await expect(mainPage.tableHeader.street).toBeVisible();
-    await expect(mainPage.tableHeader.rooms).toBeVisible();
-    await expect(mainPage.tableHeader.price).toBeVisible();
-    await expect(mainPage.tableHeader.status).toBeVisible();
-    await mainPage.newItemButton.click();
-    await expect(mainPage.editForm.name).toBeVisible();
-    await expect(mainPage.editForm.street).toBeVisible();
-    await expect(mainPage.editForm.rooms).toBeVisible();
-    await expect(mainPage.editForm.price).toBeVisible();
-    await expect(mainPage.editForm.status).toBeVisible();
-    await expect(mainPage.cancelBtn).toBeEnabled();
-    await expect(mainPage.saveBtn).toBeDisabled();
-});
-
-test.describe("UI: Create new advertisement", async () => {
-    test("New element with required fields only", async ({ page }) => {
-        const mainPage = new MainPage(page);
-        await mainPage.goto();
-
-        const reqName = "ui_name";
-        const reqPrice = "555";
-
-        await mainPage.newItemButton.click();
-        await mainPage.editForm.name.fill(reqName);
-        await mainPage.editForm.price.fill(reqPrice);
-        await mainPage.saveBtn.click();
-        await mainPage.savePopUp.waitFor({
-            state: "visible",
-        });
-        await mainPage.newItemButton.waitFor();
-        let rows = mainPage.tableRows;
-        let item = rows.filter({ hasText: "ui_name" }).filter({ hasText: "555" });
-        let itemTexts = await item.allTextContents();
-        expect(itemTexts[0]).toContain(reqName);
-        expect(itemTexts[0]).toContain(reqPrice);
+    test("Sign Up test", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto(credentials.url,credentials.userID,credentials.password);
+        await loginPage.SignUpTest();
+        //expect(accessibilityScanResults.violations).toEqual([]);
     });
-    test("New element with all fields", async ({ page }) => {
-        const mainPage = new MainPage(page);
-        await mainPage.goto();
 
-        const reqName = "ui_name";
-        const reqStreet = "ui_street";
-        const reqRooms = "393";
-        const reqPrice = "7777";
-        const reqStatus = "Active";
+    test("Log Out after Login test", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto(credentials.url,credentials.userID,credentials.password);
+        await loginPage.logOutAfterLogin();
 
-        await mainPage.newItemButton.click();
-        await mainPage.editForm.name.fill(reqName);
-        await mainPage.editForm.street.fill(reqStreet);
-        await mainPage.editForm.rooms.fill(reqRooms);
-        await mainPage.editForm.price.fill(reqPrice);
-        await mainPage.editForm.status.check();
-        await mainPage.saveBtn.click();
-        await mainPage.savePopUp.waitFor({
-            state: "visible",
-        });
-        await mainPage.newItemButton.waitFor();
-        let rows = mainPage.tableRows;
-        let item = rows.filter({ hasText: "ui_name" }).filter({ hasText: "ui_street" });
-        let itemTexts = await item.allTextContents();
-        expect(itemTexts[0]).toContain(reqName);
-        expect(itemTexts[0]).toContain(reqStreet);
-        expect(itemTexts[0]).toContain(reqRooms);
-        expect(itemTexts[0]).toContain("7.777,00");
-        expect(itemTexts[0]).toContain(reqStatus);
+        //expect(accessibilityScanResults.violations).toEqual([]);
     });
-});
 
-test("Edit any existing advertisement", async ({ page }) => {
-    const mainPage = new MainPage(page);
-    await mainPage.goto();
-
-    const reqName = "new_name";
-    const reqPrice = "343";
-    const editName = "edit_name";
-    const editPrice = "901";
-
-    await mainPage.newItemButton.click();
-    await mainPage.editForm.name.fill(reqName);
-    await mainPage.editForm.price.fill(reqPrice);
-    await mainPage.saveBtn.click();
-    await mainPage.savePopUp.waitFor({
-        state: "visible",
+    test("Log Out after Login back home test", async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.goto(credentials.url,credentials.userID,credentials.password);
+        await loginPage.logOutAfterGoingBackToHome();
+        //expect(accessibilityScanResults.violations).toEqual([]);
     });
-    await mainPage.newItemButton.waitFor();
-    let rows = mainPage.tableRows;
-    let item = rows.filter({ hasText: reqName }).filter({ hasText: reqPrice });
-    let itemTexts = await item.allTextContents();
-    expect(itemTexts[0]).toContain(reqName);
-    expect(itemTexts[0]).toContain(reqPrice);
 
-    await item.click();
-    await mainPage.editForm.name.fill(editName);
-    await mainPage.editForm.price.fill(editPrice);
-    await mainPage.saveBtn.click();
-    await mainPage.savePopUp.waitFor({
-        state: "visible",
-    });
-    await mainPage.newItemButton.waitFor();
-    let editRows = mainPage.tableRows;
-    let editItem = editRows.filter({ hasText: editName }).filter({ hasText: editPrice });
-    let editItemTexts = await editItem.allTextContents();
-    expect(editItemTexts[0]).toContain(editName);
-    expect(editItemTexts[0]).toContain(editPrice);
-});
-
-test.describe("UI: Input validators", async () => {
-    test("Advertisement name", async ({ page }) => {
-        const mainPage = new MainPage(page);
-        await mainPage.goto();
-        await mainPage.newItemButton.click();
-
-        await mainPage.editForm.name.fill("111111111111111111111111111111111111111111111111111");
-        await mainPage.editForm.street.click();
-        await expect(page.locator('text="Max length reached"').first()).toBeVisible();
-        await expect(mainPage.saveBtn).toBeDisabled();
-
-        await mainPage.editForm.name.fill("");
-        await mainPage.editForm.street.click();
-        await expect(page.locator('text="This is required"').first()).toBeVisible();
-        await expect(mainPage.saveBtn).toBeDisabled();
-    });
-    test("Price", async ({ page }) => {
-        const mainPage = new MainPage(page);
-        await mainPage.goto();
-        await mainPage.newItemButton.click();
-
-        await mainPage.editForm.price.fill("565,3411");
-        await mainPage.editForm.street.click();
-        await expect(page.locator('text="Invalid price(Valid currency in euros: 12,12)"').first()).toBeVisible();
-        await expect(mainPage.saveBtn).toBeDisabled();
-
-        await mainPage.editForm.price.fill("");
-        await mainPage.editForm.street.click();
-        await expect(page.locator('text="This is required"').first()).toBeVisible();
-        await expect(mainPage.saveBtn).toBeDisabled();
-    });
+    
 });
