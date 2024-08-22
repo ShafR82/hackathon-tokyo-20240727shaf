22612e311278a6cc8eeb4d6681c511d3b84c8a62
ShafR82 <Shafiq.82@outlook.com>
2024-08-22 17:14:16 +0900

    Added new page model for the User Stories Page and one new test which uses it

--- tests/shaf.spec.js
+++ tests/shaf.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require("@playwright/test");
 const path = require('path');
 
 const fs = require('fs');
+const { UserStoriesPage } = require("../lib/pageobjects/UserStoriesPage");
 const projectPath = process.cwd();
 
 console.log("Project Path: %s",projectPath);
@@ -12,19 +13,29 @@ console.log("File Path: %s",filePath);
 // Read credentials from JSON file
 const credentials = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-test.describe("ui functional Test", () => {
+// Here you'll find my first Playwright tests  
+test.describe("First playwright Test", () => {
 
-    //SHAF: My first Playwright test 
-    test("My first Playwright test", async ({ page }) => {
+    /*SHAF: My first Playwright test, the goal of this test is to check the "User Stories" page's title
+    , the title should be "User Stories | Todos App". Also I'm not using the page objects created by 
+    Subhajit on purpose as I want to learn step by step :-)*/
+    test("User Stories page title test", async ({ page }) => {
         console.log("Test Name: My first Playwright test!");
         await page.goto("https://todo-app-qajp.vercel.app/");
-        await page.waitForLoadState('domcontentloaded');
         await page.getByRole('link', { name: 'User Stories' }).click();
-        //await page.waitForURL('https://todo-app-qajp.vercel.app/userstories', "domcontentloaded");   
         await page.waitForTimeout(1000);   //NOT RECOMMENDED NEED FIX  
         let titre = await page.title();
         console.log("Titre: %s", titre);
         expect(titre).toContain("User Stories | Todos App");
     });
+
+    test("User Stories Content", async({page}) => {
+        console.log("User Stories content");
+        const userStories = new UserStoriesPage(page);
+        await userStories.UserStories();
+        /*not really testing the content of the page here but just for testing purpose I'll check the title
+        like in my previous test*/
+        expect(await userStories.page.title()).toContain("User Stories | Todos App");  
+    }); 
     
 });
